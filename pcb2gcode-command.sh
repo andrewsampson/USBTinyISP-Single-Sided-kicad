1664a7d61a56ca1c1cc49f4691fe271333f78f49
Andrew Sampson <andrew@tyndall>
2013-02-02 19:35:01 -0600

    Removed the extra cutting pass argument from the pcb2gcode script

--- pcb2gcode-command.sh
+++ pcb2gcode-command.sh
@@ -3,6 +3,8 @@
 # From previous project, found that the width of the groove is closer to
 # 0.3 mm or so, which means offset should be half that.
 
+# also found out that an extra pass isn't necessary for this board
+
 ~/proj/pcb2gcode-1.1.4/build/pcb2gcode \
 	--metric \
 	--zwork -0.01 \
@@ -10,7 +12,6 @@
 	--zchange 33 \
 	--zdrill -1.7 \
 	--offset 0.2 \
-	--extra-passes 1 \
 	--mill-feed 200 \
 	--mill-speed 3000 \
 	--drill-feed 50 \
